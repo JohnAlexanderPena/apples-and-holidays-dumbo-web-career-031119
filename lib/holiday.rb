3bfb5b608489b89088d10a8ebc35ed414cded3e5
John Pena <jpena91@gmail.com>
2019-02-24 19:15:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -36,7 +36,7 @@ end
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   holiday_hash.each do |season, value|
     if season == :thanksgiving
-      [:thanksgiving][:holiday_name]= supply_array
+      [:thanksgiving][holiday_name]= supply_array
     end 
   end 
 end
