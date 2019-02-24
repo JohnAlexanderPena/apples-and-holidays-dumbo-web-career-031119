9d64d4f032f7a6b290488ebd1acf493741772315
John Pena <jpena91@gmail.com>
2019-02-24 19:39:28 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -37,7 +37,9 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   holiday_hash.each do |key, value|
     if season == :fall 
       [:fall].each do |key , value|
-        value.merge! ({})
+        value.merge! ([holiday_name]= supply_array)
+      end 
+    end 
 end
 
 def all_winter_holiday_supplies(holiday_hash)
