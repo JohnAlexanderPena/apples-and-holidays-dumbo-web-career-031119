901e413897ce453baacfbb84eb5c0ec9011b8155
John Pena <jpena91@gmail.com>
2019-02-24 19:04:33 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -29,7 +29,6 @@ end
 
 def add_supply_to_memorial_day(holiday_hash, supply)
  holiday_hash[:spring].update(:memorial_day => :spring[:memorial_day].insert{|item| item == supply})
-  end 
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
